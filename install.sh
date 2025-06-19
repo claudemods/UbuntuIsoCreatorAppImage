5e979b02c5d015cd0d4e657fc7743a3c5a77bbd8
claudemods <73653396+claudemods@users.noreply.github.com>
2025-06-19 15:13:16 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+# Fancy Loading Bar Function (Cyan #24ffff)
+function loading_bar() {
+    local duration=${1:-3.7}  # Default: 3.7 seconds
+    local width=50
+    local progress=0
+    local increment=$((100 * 10 / (duration * 10) / width))
+
+    echo -ne "\e[38;2;36;255;255m"  # Set color to #24ffff
+    while [ $progress -le 100 ]; do
+        local filled=$((progress * width / 100))
+        local empty=$((width - filled))
+        printf "\r["
+        printf "%${filled}s" | tr ' ' '█'
+        printf "%${empty}s" | tr ' ' ' '
+        printf "] %3d%%" $progress
+        sleep 0.1
+        ((progress += increment))
+    done
+    echo -e "\e[0m"  # Reset color
+}
+
+# Show loading bar at start
+echo -e "\e[38;2;36;255;255mInitializing setup...\e[0m"
+loading_bar 3.7
+
+# Clone Repo
+git clone https://github.com/claudemods/UbuntuIsoCreatorAppImage.git || { 
+    echo -e "\e[31mFailed to clone repo!\e[0m"
+    exit 1
+}
+
+# Compile Components (YOUR EXACT COMMANDS)
+cd UbuntuIsoCreatorAppImage/cscript/MainApp && make || exit 1
+cd UbuntuIsoCreatorAppImage/cscript/SetupScript && make || exit 1
+cd UbuntuIsoCreatorAppImage/cscript/isocreator && make || exit 1
+cd UbuntuIsoCreatorAppImage/cscript/SquashfsCreator && make || exit 1
+
+# User input for build type
+while true; do
+    read -p "Choose build type (noble/oracular): " build_type
+    case "$build_type" in
+        noble|oracular)
+            echo -e "\e[32mSelected: $build_type\e[0m"
+            break
+            ;;
+        *)
+            echo -e "\e[31mInvalid choice! Please enter 'noble' or 'oracular'.\e[0m"
+            ;;
+    esac
+done
+
+# Handle build image
+cd UbuntuIsoCreatorAppImage/cscript/Build-Images || exit 1
+if [ -f "build-image-$build_type.zip" ]; then
+    echo -e "\e[38;2;36;255;255mExtracting build-image-$build_type.zip...\e[0m"
+    mkdir -p /home/$USER/.config/UbuntuIsoCreatorAppImage/cscript/Build-Images || exit 1
+    unzip -q "build-image-$build_type.zip" -d /home/$USER/.config/UbuntuIsoCreatorAppImage/cscript/Build-Images/ || exit 1
+else
+    echo -e "\e[31mError: build-image-$build_type.zip not found!\e[0m"
+    exit 1
+fi
+
+# COPY ONLY COMPILED FILES (not the whole cscript directory)
+echo -e "\e[38;2;36;255;255mCopying compiled binaries...\e[0m"
+mkdir -p /home/$USER/.config/cui || {
+    echo -e "\e[31mFailed to create /home/$USER/.config/cui!\e[0m"
+    exit 1
+}
+
+# Copy each compiled executable individually
+cp UbuntuIsoCreatorAppImage/cscript/MainApp/mainapp /home/$USER/.config/cui/ || cp_failed=1
+cp UbuntuIsoCreatorAppImage/cscript/SetupScript/setupscript /home/$USER/.config/cui/ || cp_failed=1
+cp UbuntuIsoCreatorAppImage/cscript/isocreator/isocreator /home/$USER/.config/cui/ || cp_failed=1
+cp UbuntuIsoCreatorAppImage/cscript/SquashfsCreator/squashfscreator /home/$USER/.config/cui/ || cp_failed=1
+
+if [ "$cp_failed" = 1 ]; then
+    echo -e "\e[31mFailed to copy one or more compiled files!\e[0m"
+    exit 1
+fi
+
+echo -e "\e[1;32mInstallation completed successfully!\e[0m"
+sudo cp UbuntuIsoCreatorAppImage/cscript/cui.desktop /usr/share/applications
+echo you can launch cui.desktop from applications menu
